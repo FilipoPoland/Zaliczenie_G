8e78df9c706f59df559d2e73bba50cdd40791cf8
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-04-13 19:02:09 +0200

    Commit

--- Zadanie1.py
+++ Zadanie1.py
@@ -4,26 +4,32 @@
 # podaj ile jest miejsc zerowych w komunikacie
 
 from math import sqrt
+c = True
+while c:
+    u_input = input('Podaj współczynniki funkcji kwadratowej odzielone przecinkami w klejności a, b, c (separator '
+                    'dziesiętny to kropka): ')
+    # w razie gdyby użytkownik podał spacje
+    u_input = u_input.replace(' ', '')
+    # stworzenie listy
+    u_list = u_input.split(',')
 
-u_input = input('Podaj współczynniki funkcji kwadratowej odzielone przecinkami w klejności a, b, c (separator '
-                'dziesiętny to kropka): ')
-# w razie gdyby użytkownik podał spacje
-u_input = u_input.replace(' ', '')
-# stworzenie listy
-u_list = u_input.split(',')
+    try:
+        delta = (float(u_list[1]) ** 2) - (4 * float(u_list[0]) * float(u_list[2]))
+        if delta < 0:
+            print('Brak miejsc zerowych.')
 
-delta = (float(u_list[1]) ** 2) - (4 * float(u_list[0]) * float(u_list[2]))
+        elif delta == 0:
 
-if delta < 0:
-    print('Brak miejsc zerowych.')
+            print('Jedno miejsce zerowe')
+        elif delta > 0:
+            x1 = (-(float(u_list[1])) + sqrt(delta)) / (2 * float(u_list[0]))
+            x2 = (-(float(u_list[1])) - sqrt(delta)) / (2 * float(u_list[0]))
+            # zaokraglenie
+            x1 = round(x1, 2)
+            x2 = round(x2, 2)
+            print(f'Dwa miejscza zerowe: {x1} oraz {x2}')
+            c = False
+    except:
+        print('Zostały podane błędne wartości.')
 
-elif delta == 0:
 
-    print('Jedno miejsce zerowe')
-elif delta > 0:
-    x1 = (-(float(u_list[1])) + sqrt(delta)) / (2 * float(u_list[0]))
-    x2 = (-(float(u_list[1])) - sqrt(delta)) / (2 * float(u_list[0]))
-    # zaokraglenie
-    x1 = round(x1, 2)
-    x2 = round(x2, 2)
-    print(f'Dwa miejscza zerowe: {x1} oraz {x2}')
