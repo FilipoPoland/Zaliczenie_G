795cb701e387e72bf353f0ff1bbd934a612b905d
FilipoPoland <filipopoland@wp.pl>
2023-04-27 10:20:37 +0200

    Commit

--- cz2/Zadanie1.py
+++ cz2/Zadanie1.py
@@ -1,13 +1,19 @@
+from random import randint
+
+a = -3.22
+b = 2.91
+
+# zapytanie uzytkownika
 while True:
     while True:
         try:
-            dol = int(input('Podaj dolna wartosc zakresu: '))
+            dol = int(input('Podaj dolna wartosc zakresu dla możliwego c: '))
             break
         except:
             continue
     while True:
         try:
-            gora = int(input('Podaj gorna wartosc zakresu: '))
+            gora = int(input('Podaj gorna wartosc zakresu dla możliwego c: '))
             break
         except:
             continue
@@ -15,3 +21,19 @@ while True:
         break
 
 
+c = randint(dol, gora)
+
+# stworzenie listy y
+ylist = []
+xlist = []
+# range okresla przedzial od -20 do 25 po prawej nawias otwarty
+# 5 to skok
+for i in range(-20, 25, 5):
+    xlist.append(i)
+    ylist.append(a*(i**2)+(b*i)+c)
+
+# wyswietlenie wynikow
+x = 0
+for i in ylist:
+    print(f'\nx: {xlist[x]}\ny: {i}')
+    x += 1
