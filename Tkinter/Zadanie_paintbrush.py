032b3299a8334b3e75b196a7196dc13b10e941d8
FilipoPoland <40774085+FilipoPoland@users.noreply.github.com>
2023-06-13 16:15:08 +0200

    Commit

---
+++ Tkinter/Zadanie_paintbrush.py
@@ -0,0 +1,30 @@
+from tkinter import *
+import random
+
+
+def maluj(event):
+    kolory = podaj_kolor.get()
+    kolory = kolory.split(', ')
+    print(kolory)
+    kolor = random.choice(kolory)
+    print(kolor)
+    obraz.create_rectangle(event.x, event.y, event.x, event.y, fill=kolor, outline=kolor)
+
+
+def usun(event):
+    obraz.create_rectangle(event.x, event.y, event.x, event.y, fill='white', outline='white')
+
+
+okno = Tk()
+okno.geometry('600x600')
+
+obraz = Canvas(okno, width=500, height=500, bg='white')
+obraz.pack()
+
+podaj_kolor = Entry(okno)
+podaj_kolor.pack()
+
+obraz.bind('<B1-Motion>', maluj)
+obraz.bind('<B3-Motion>', usun)
+
+okno.mainloop()
